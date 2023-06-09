69b3a202e5c3095a79982e68c315184a42a3bbe4
Haley T <hntrimpert@yahoo.com>
2023-06-09 14:27:07 -0400

    server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Employee-Tracker
 Module 12 Challenge
+
+## Description
+
+This is an employee tracker application. Using Inquirer, MySQL and console.table.
+
+## Link
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    password: "",
+    database: "department_db",
+  },
+  console.log(`Connected to the department_db database.`)
+);
+
+app.get("/api/department", (req, res) => {
+  const sql = `SELECT id, department_name AS title FROM department`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+
