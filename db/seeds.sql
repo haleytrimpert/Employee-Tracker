efb85d2037dd698231b8a6d3349d7e8b851d26fb
Haley T <hntrimpert@yahoo.com>
2023-06-09 14:21:18 -0400

    seeds done

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (department_name)
+VALUES ("HR"),
+        ("ACcounting"),
+        ("Sales"),
+        ("Customer Service");
+
+INSERT INTO role (title, salary)
+VALUES ("HR rep", "70000"),
+        ("HR manager", "100000"),
+        ("Accountant", "100000"),
+        ("Sales Lead", "60000"),
+        ("Sales Representative", "50000"),
+        ("Customer Service Manager", "50000"),
+        ("Customer Service Representative", "40000");
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+        ("Mike", "Chan", 2),
+        ("Ashley", "Rodriguez", 3),
+        ("Kevin", "Tupik", 4),
+        ("Kunal", "Singham", 5),
+        ("Malia", "Brown", 6),
+        ("Sarah", "Lourde", 7);        
